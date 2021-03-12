d55335ac2d1a8d885e61f218b85b7dca0baa2430
AlexandrKudrya <70281496+AlexandrKudrya@users.noreply.github.com>
2021-03-12 18:45:41 +0900

    Add files via upload

--- index.py
+++ index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('base.html', title='Заготовка')
 
+@app.route("/training/<prof>")
+def training(prof):
+    return render_template('training.html', title='Заготовка', prof=prof)
+
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
